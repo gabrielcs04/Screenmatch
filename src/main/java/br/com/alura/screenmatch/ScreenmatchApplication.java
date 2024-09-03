192c5ee9b3a345e0ee0030d4ece8e54220ed88ea
Gabriel Costa da Silva <gabriel.costasilva04@gmail.com>
2024-09-03 20:26:24 -0300

    Modeling episode and season data

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,15 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.SeriesData;
+import br.com.alura.screenmatch.model.SeasonData;
+import br.com.alura.screenmatch.model.SerieData;
 import br.com.alura.screenmatch.service.ApiConsumption;
 import br.com.alura.screenmatch.service.ConvertsData;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -17,11 +21,23 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var apiConsumption = new ApiConsumption();
+
 		var json = apiConsumption.getData("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
 		System.out.println(json);
 
-		ConvertsData conversor = new ConvertsData();
-		SeriesData data = conversor.getData(json, SeriesData.class);
-		System.out.println(data);
+		ConvertsData converter = new ConvertsData();
+
+		SerieData serieData = converter.getData(json, SerieData.class);
+		System.out.println(serieData);
+
+		List<SeasonData> seasons = new ArrayList<>();
+
+		for (int i = 1; i <= serieData.totalSeasons(); i++) {
+			json = apiConsumption.getData("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=6585022c");
+			SeasonData seasonData = converter.getData(json, SeasonData.class);
+			seasons.add(seasonData);
+		}
+
+		seasons.forEach(System.out::println);
     }
 }
--- src/main/java/br/com/alura/screenmatch/model/SerieData.java
+++ src/main/java/br/com/alura/screenmatch/model/SerieData.java
@@ -4,8 +4,7 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record SeriesData(@JsonAlias("Title") String title,
-                         @JsonAlias("totalSeasons") Integer totalSeasons,
-                         @JsonAlias("imdbRating") String rating) {
-
+public record SerieData(@JsonAlias("Title") String title,
+                        @JsonAlias("totalSeasons") Integer totalSeasons,
+                        @JsonAlias("imdbRating") String rating) {
 }
